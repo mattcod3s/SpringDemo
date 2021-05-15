81fd2821982bf68751dfaeb56a297b2c16babcdb
MattCod3s <matthew.kostka08@gmail.com>
2021-05-15 19:58:03 +0100

    Added Post Request to database and handled copies

--- src/main/java/com/example/demo/Student/StudentService.java
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.demo.Student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,16 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
-
     public List<Student> getStudents(){
 		return studentRepository.findAll();
 	}
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+        System.out.println(student);
+    }
 }
